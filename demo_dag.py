92a5a91ff6ecfc99c9e3c2d141c2a2b611033adc
Laaziz <laazizahmed72@gmail.com>
2023-07-09 17:24:51 +0200

    udpate

--- demo_dag.py
+++ demo_dag.py
@@ -1,23 +1,3 @@
-# from datetime import datetime
-
-# from airflow import DAG
-# from airflow.decorators import task
-# from airflow.operators.bash import BashOperator
-
-# # A DAG represents a workflow, a collection of tasks
-# with DAG(dag_id="demo", start_date=datetime(2023, 7, 9), schedule="0 0 * * *") as dag:
-
-#     # Tasks are represented as operators
-#     hello = BashOperator(task_id="hello", bash_command="echo hello")
-
-#     @task()
-#     def airflow():
-#         print("airflow")
-
-#     # Set dependencies between tasks
-#     hello >> airflow()
-
-
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash import BashOperator
